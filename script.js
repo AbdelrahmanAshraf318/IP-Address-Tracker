fe0966a527fc0dae0894bbd1cd046bc3a34eba8f
Abdelrahman Ashraf <abdelrahmanashraf25102000@gmail.com>
2021-04-29 15:21:38 +0200

    Changing style and add the first version of API code

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-const api = 'https://geo.ipify.org/';
-const theMap = 'https://leafletjs.com/';
+let api = 'http://api.icndb.com/jokes/random/3';
+
+
+window.addEventListener('load' , () => {
+    console.log("random");
+    fetch(api).then(function(response){
+        return response.json()
+    })
+    .then(function(json){
+        for(var i=0 ; i<json.value.length ; i++){
+            console.log(json.value[i].joke);
+        }
+    })
+})
+
+
+
